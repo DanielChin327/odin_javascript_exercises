0eca5aab5555a65ef372268ff37ea75b6deb44e2
Daniel Chin <DanChin327@gmail.com>
2024-04-08 15:41:11 +0900

    completed 04 remove from Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  let result = [];
 
+  array.forEach((num) => {
+    if (!args.includes(num)) {
+      result.push(num);
+    }
+  });
+
+  return result;
 };
 
 // Do not edit below this line
