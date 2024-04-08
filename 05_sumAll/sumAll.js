62f289db4754f531ede41f067cc98b4b4e3deb1d
Daniel Chin <DanChin327@gmail.com>
2024-04-08 16:18:56 +0900

    finished 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let start = num1;
+  let end = num2;
+  let result = 0;
+if (typeof num1 != "number" || typeof num2 != "number") {
+  return "ERROR"
+}
+else if (num1 < 0 || num2 < 0) {
+  return "ERROR"
+}
 
+if (num1 > num2) {
+  start = num2;
+  end = num1;
+}
+
+for (let i = start; i <= end; i++) {
+  result = result + i;
+}
+
+return result;
 };
 
 // Do not edit below this line
