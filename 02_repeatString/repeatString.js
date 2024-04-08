fe42a875c84bc71e0daf6ee1088e20294a3cce0d
Daniel Chin <DanChin327@gmail.com>
2024-04-08 15:04:00 +0900

    completed 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+  if (num < 1){
+    return "ERROR"
+  }
+  else {
+    let result = ''
+    for (let i = 1; i <= num; i++){
+      result = result + string;
+    }
+    return result;
+  }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
