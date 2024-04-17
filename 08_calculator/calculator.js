93aa728350ae3e27d6001accb74bc61f0a93b201
Daniel Chin <DanChin327@gmail.com>
2024-04-17 22:10:07 +0900

    finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array1) {
+  return array1.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array1) {
+  return array1.reduce((total, current) => total * current, 0)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
