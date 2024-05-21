85b1060e60551d9add91b7db609ba48031653a53
Daniel Chin <DanChin327@gmail.com>
2024-05-21 12:51:25 -0600

    finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,10 @@
-const reverseString = function(string) {
-let result = '';
-
-for (let i = string.length - 1; i >= 0; i--) {
-  result = result + string[i];
+const reverseString = (string) => {
+  let result = '';
+  for (let i = string.length-1; i >= 0; i--){
+    result += string[i]
+  }
+  return result
 }
-return result;
-};
 
 // Do not edit below this line
 module.exports = reverseString;
