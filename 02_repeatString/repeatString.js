beb8b9ccac3de63fafd08f86a635f4c6d0d3fffa
Daniel Chin <DanChin327@gmail.com>
2024-05-21 12:19:10 -0600

    completed JS exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function(string, num) {
-
+const repeatString = (string, num) => {
+  if (num < 0) {
+    return "ERROR"
+  }
+  let message = '';
+  for (let i = 0; i < num; i++){
+    message += string
+  }
+  return message
 }
 
 // Do not edit below this line
