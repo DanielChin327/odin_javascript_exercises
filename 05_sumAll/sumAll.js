3ea5c795c95136682a8e6b0868cefad5c4c23772
Daniel Chin <DanChin327@gmail.com>
2024-05-21 19:11:01 -0600

    finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,25 +1,24 @@
-const sumAll = function(num1, num2) {
-  let start = num1;
-  let end = num2;
+const sumAll = (num1, num2) => {
+
+  if(!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR"
+  }
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR"
+  }
+
+  if(num1 > num2){
+    let temp = num1;
+    num1 = num2
+    num2 = temp
+  }
+
   let result = 0;
-if (typeof num1 != "number" || typeof num2 != "number") {
-  return "ERROR"
+  for (let i = num1; i <= num2; i++) {
+    result += i
+  }
+  return result;
 }
-else if (num1 < 0 || num2 < 0) {
-  return "ERROR"
-}
-
-if (num1 > num2) {
-  start = num2;
-  end = num1;
-}
-
-for (let i = start; i <= end; i++) {
-  result = result + i;
-}
-
-return result;
-};
 
 // Do not edit below this line
 module.exports = sumAll;
