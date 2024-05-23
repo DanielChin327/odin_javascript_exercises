e9880ff41422bdabe86e0ea7114cba0c94b24e82
Daniel Chin <DanChin327@gmail.com>
2024-05-22 18:06:21 -0600

    ongoing palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+ let lowercase = string
+ .toLowerCase()   // Convert to lowercase
+ .split('') // Split by one or more spaces
+ .filter(character => character !== ' '); // Filter out spaces
+
 
 };
 
